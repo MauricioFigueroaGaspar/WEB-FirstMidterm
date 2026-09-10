1a413bb0c0ac416ab99eeb4227c8cc8fee4e6d39
Adrian <adrian.undertale@gmail.com>
2026-09-10 17:13:05 -0600

    First prototype of integration with html and js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-// El costo original
-const costInput = parseFloat(document.getElementById("cost").value);
 // Los radios para seleccionar los valores de la propina
-const radio1 = document.getElementById("radio1");
-const radio2 = document.getElementById("radio2");
-const radio3 = document.getElementById("radio3");
-const radio4 = document.getElementById("radio4");
-const radio5 = document.getElementById("radio5");
-const radio6 = document.getElementById("radio6");
-// La variable para tener el "input" de la propina custom
-const customRadioInput = document.getElementById("customTip");
-// El numero de personas
-const numberOfPeopleInput = parseFloat(document.getElementById("number-of-people").value);
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+let tipInput = document.getElementById("customTip");
 // Boton para el reset
 const resetButton = document.getElementById("resetButton")
 // El valor de la propina
@@ -22,30 +16,31 @@ let totalPerPerson = 0.0;
 
 
     // Dependiendo de cual opción esta seleccionada, cambiara el valor del tip
-    radio1.addEventListener("change", (e) => { if (e.target.checked) tip = .5; });
-    radio2.addEventListener("change", (e) => { if (e.target.checked) tip = .10; });
-    radio3.addEventListener("change", (e) =>{ if (e.target.checked) tip = .15; });
-    radio4.addEventListener("change", (e) =>{ if (e.target.checked) tip = .25; });
-    radio5.addEventListener("change", (e) =>{ if (e.target.checked) tip = .50; });
+    button1.addEventListener("click", (e) => { tip = .5; tipInput.value = ""; });
+    button2.addEventListener("click", (e) => { tip = .10; tipInput.value = ""; });
+    button3.addEventListener("click", (e) =>{ tip = .15; tipInput.value = ""; });
+    button4.addEventListener("click", (e) =>{ tip = .25; tipInput.value = ""; });
+    button5.addEventListener("click", (e) =>{ tip = .50; tipInput.value = ""; });
 
     // La siguiente opcion es para el radio con la propina custom
-    radio6.addEventListener("change", (e) => {
-       if (e.taget.checked){
-        customTipInput.style.display = "inline.block";
-        tip = (paseFloat(customTipInput.value) || 0) / 100.0;
-       } else {
-        customTipInput.style.display = "";
-       }
+    tipInput.addEventListener("input", (e) => {
+        tipInput.style.display = "inline.block";
+        tip = (parseFloat(tipInput.value) || 0) / 100.0;
     });
 
 // Calcular la propina ya teniendo los valores
 function updateData(){
-    if(costInput > 0 && numberOfPeopleInput > 0 ){
+    // El costo original
+    const costInput = parseFloat(document.getElementById("cost").value);
+    // El numero de personas
+    const numberOfPeopleInput = parseFloat(document.getElementById("number-of-people").value);
+
+    if(costInput > 0 && numberOfPeopleInput > 0 && tip > 0){
         tipPerPerson = (costInput*tip)/numberOfPeopleInput;
         totalPerPerson = (costInput + (costInput*tip))/numberOfPeopleInput;
         // Integrar los valores al html
-        document.getElementById("tipAmountPerPerson").innerHTML = "" + tipPerPerson;
-        document.getElementById("totalPerPerson").innerHTML = "" + totalPerPerson;
+        document.getElementById("tipAmountPerPerson").innerHTML = "$" + tipPerPerson;
+        document.getElementById("totalPerPerson").innerHTML = "$" + totalPerPerson;
     }
 }
 // Definir el intervalo para que se se repita la funcion

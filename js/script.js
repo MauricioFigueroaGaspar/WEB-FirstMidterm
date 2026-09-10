74770f489d0bf02da923b9ec447980efd8519916
Adrian <adrian.undertale@gmail.com>
2026-09-10 12:45:26 -0600

    Implemented an interval so the data is updated and changed how some consts are called

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // El costo original
 const costInput = parseFloat(document.getElementById("cost").value);
 // Los radios para seleccionar los valores de la propina
-const radio1 = document.querySelector("#radio1");
-const radio2 = document.querySelector("#radio2");
-const radio3 = document.querySelector("#radio3");
-const radio4 = document.querySelector("#radio4");
-const radio5 = document.querySelector("#radio5");
-const radio6 = document.querySelector("#radio6");
+const radio1 = document.getElementById("radio1");
+const radio2 = document.getElementById("radio2");
+const radio3 = document.getElementById("radio3");
+const radio4 = document.getElementById("radio4");
+const radio5 = document.getElementById("radio5");
+const radio6 = document.getElementById("radio6");
 // La variable para tener el "input" de la propina custom
-const customRadioInput = document.querySelector("#customTip");
+const customRadioInput = document.getElementById("customTip");
 // El numero de personas
 const numberOfPeopleInput = parseFloat(document.getElementById("number-of-people").value);
 // Boton para el reset
@@ -39,13 +39,17 @@ let totalPerPerson = 0.0;
     });
 
 // Calcular la propina ya teniendo los valores
-if(costInput > 0 && numberOfPeopleInput > 0 ){
-    tipPerPerson = (costInput*tip)/numberOfPeopleInput;
-    totalPerPerson = (costInput + (costInput*tip))/numberOfPeopleInput;
-    // Integrar los valores al html
-    document.getElementById("tipAmountPerPerson").innerHTML = "" + tipPerPerson;
-    document.getElementById("totalPerPerson").innerHTML = "" + totalPerPerson;
+function updateData(){
+    if(costInput > 0 && numberOfPeopleInput > 0 ){
+        tipPerPerson = (costInput*tip)/numberOfPeopleInput;
+        totalPerPerson = (costInput + (costInput*tip))/numberOfPeopleInput;
+        // Integrar los valores al html
+        document.getElementById("tipAmountPerPerson").innerHTML = "" + tipPerPerson;
+        document.getElementById("totalPerPerson").innerHTML = "" + totalPerPerson;
+    }
 }
+// Definir el intervalo para que se se repita la funcion
+const repeatingFunctions = setInterval(updateData, 200);
 
     // Resetear todos los valores al presionar el boton de RESET
     resetButton.addEventListener("click", (e) => { location.reload(); });
